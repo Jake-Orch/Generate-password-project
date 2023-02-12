8b057c976ef5c405db2b1c76d746c9b0e13883b0
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-02-12 18:14:21 +0000

    if info added to generatePassword function

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-var allLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "X", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var allNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var allSpecial = ["!", "#", "$", "%", "&", "&", "*", "+", "-", "/", "_", ":", ";", "=", "<", ">", "?", "@", "]", "[", "(", ")", "{", "}", "~"];
+var allLetters = ["abcdefghijklmnopqrstuvwxyz"];
+var allCaps = ["ABCDEFGHIJKLMNOPRXTUVWXYZ"];
+var allNumbers = ["0123456789"];
+var allSpecial = ["!#$%&&*+-/_:;=<>?@][(){}~"];
 
 function affirmData() {
   console.log("generatePassword function is running");
@@ -17,12 +18,30 @@ function affirmData() {
   special;
   length;
   console.log("Inside affirmData " + capital + " " + numbers + " " + special + " " + length)
+  generatePassword(capital, numbers, special, length);
   return [capital, numbers, special, length];
 }
 
-function generatePassword() {
+function generatePassword(capital, numbers, special, length) {
   console.log("Inside generatePassword " + capital + " " + numbers + " " + special + " " + length)
+  if (capital == true && numbers == true && special == true)
+  console.log("all true")
+  else if (capital == false && numbers == true && special == true)
+  console.log("all true but capital")
+  else if (capital == true && numbers == false && special == true)
+  console.log("all true but numbers")
+  else if (capital == true && numbers == true && special == false)
+  console.log("all true but special")
+  else if (capital == true && numbers == false && special == false)
+  console.log("all false but capital")
+  else if (capital == false && numbers == true && special == false)
+  console.log("all false but numbers")
+  else if (capital == false && numbers == false && special == true)
+  console.log("all false but special")
+  else
+  console.log("all false")
 }
+//write for loops, to go through the selected groups of characters, and the amount of loops is equal to the specified length
 
 function writePassword() {
   console.log("writePassword is running");
